d23ad314daed2a80450ac36dec1bdb78e72eae8c
Blake Smith <besmith43@gmail.com>
2020-09-09 09:35:42 -0500

    changed the vlcArgs to have a time to live

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace TTU_VLCStream
         static void Main(string[] args)
         {
             string vlcExe;
-            string vlcArgs = "screen:// --screen-fps=30 --screen-caching=300 --screen-width=1920 --screen-height=1080 --sout=\"#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:rtp{dst=239.0.0.1,port=4570,mux=ts}\" --no-sout-all --sout-keep -I dummy";
+            string vlcArgs = @"screen:// --screen-fps=30 --screen-caching=300 --screen-width=1920 --screen-height=1080 --sout=""#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:rtp{dst=239.0.0.1,port=4570,mux=ts}"" --no-sout-all --sout-keep -I dummy --ttl 12";
 
             if (System.IO.File.Exists(@"C:\Program Files\VideoLAN\VLC\vlc.exe"))
             {
